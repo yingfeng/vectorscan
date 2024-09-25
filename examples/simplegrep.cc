bc7c12c667f1b25299a9f212caf57413bbd65a62
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2024-09-25 19:53:31 +0800

    Tiny

--- examples/simplegrep.cc
+++ examples/simplegrep.cc
@@ -57,7 +57,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <string>
+#include <iostream>
 #include <hs.h>
 
 /**
@@ -72,85 +73,8 @@ static int eventHandler(unsigned int id, unsigned long long from,
     return 0;
 }
 
-/**
- * Fill a data buffer from the given filename, returning it and filling @a
- * length with its length. Returns NULL on failure.
- */
-static char *readInputData(const char *inputFN, unsigned int *length) {
-    FILE *f = fopen(inputFN, "rb");
-    if (!f) {
-        fprintf(stderr, "ERROR: unable to open file \"%s\": %s\n", inputFN,
-                strerror(errno));
-        return NULL;
-    }
-
-    /* We use fseek/ftell to get our data length, in order to keep this example
-     * code as portable as possible. */
-    if (fseek(f, 0, SEEK_END) != 0) {
-        fprintf(stderr, "ERROR: unable to seek file \"%s\": %s\n", inputFN,
-                strerror(errno));
-        fclose(f);
-        return NULL;
-    }
-    long dataLen = ftell(f);
-    if (dataLen < 0) {
-        fprintf(stderr, "ERROR: ftell() failed: %s\n", strerror(errno));
-        fclose(f);
-        return NULL;
-    }
-    if (fseek(f, 0, SEEK_SET) != 0) {
-        fprintf(stderr, "ERROR: unable to seek file \"%s\": %s\n", inputFN,
-                strerror(errno));
-        fclose(f);
-        return NULL;
-    }
-
-    /* Hyperscan's hs_scan function accepts length as an unsigned int, so we
-     * limit the size of our buffer appropriately. */
-    if ((unsigned long)dataLen > UINT_MAX) {
-        dataLen = UINT_MAX;
-        printf("WARNING: clipping data to %ld bytes\n", dataLen);
-    } else if (dataLen == 0) {
-        fprintf(stderr, "ERROR: input file \"%s\" is empty\n", inputFN);
-        fclose(f);
-        return NULL;
-    }
-
-    char *inputData = malloc(dataLen);
-    if (!inputData) {
-        fprintf(stderr, "ERROR: unable to malloc %ld bytes\n", dataLen);
-        fclose(f);
-        return NULL;
-    }
-
-    char *p = inputData;
-    size_t bytesLeft = dataLen;
-    while (bytesLeft) {
-        size_t bytesRead = fread(p, 1, bytesLeft, f);
-        bytesLeft -= bytesRead;
-        p += bytesRead;
-        if (ferror(f) != 0) {
-            fprintf(stderr, "ERROR: fread() failed\n");
-            free(inputData);
-            fclose(f);
-            return NULL;
-        }
-    }
-
-    fclose(f);
-
-    *length = (unsigned int)dataLen;
-    return inputData;
-}
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <pattern> <input file>\n", argv[0]);
-        return -1;
-    }
-
-    char *pattern = argv[1];
-    const char *inputFN = argv[2];
 
     /* First, we attempt to compile the pattern provided on the command line.
      * We assume 'DOTALL' semantics, meaning that the '.' meta-character will
@@ -158,23 +82,19 @@ int main(int argc, char *argv[]) {
      * either return a compiled Hyperscan database, or an error message
      * explaining why the pattern didn't compile.
      */
+    std::string pattern("[a-z\\.-]+$");
+
     hs_database_t *database;
     hs_compile_error_t *compile_err;
-    if (hs_compile(pattern, HS_FLAG_DOTALL, HS_MODE_BLOCK, NULL, &database,
+    if (hs_compile(pattern.c_str(), HS_FLAG_DOTALL, HS_MODE_BLOCK, NULL, &database,
                    &compile_err) != HS_SUCCESS) {
-        fprintf(stderr, "ERROR: Unable to compile pattern \"%s\": %s\n",
-                pattern, compile_err->message);
         hs_free_compile_error(compile_err);
         return -1;
     }
 
     /* Next, we read the input data file into a buffer. */
-    unsigned int length;
-    char *inputData = readInputData(inputFN, &length);
-    if (!inputData) {
-        hs_free_database(database);
-        return -1;
-    }
+    std::string inputData("test-case");
+    unsigned int length = inputData.size();
 
     /* Finally, we issue a call to hs_scan, which will search the input buffer
      * for the pattern represented in the bytecode. Note that in order to do
@@ -195,27 +115,25 @@ int main(int argc, char *argv[]) {
     hs_scratch_t *scratch = NULL;
     if (hs_alloc_scratch(database, &scratch) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to allocate scratch space. Exiting.\n");
-        free(inputData);
         hs_free_database(database);
         return -1;
     }
 
     printf("Scanning %u bytes with Hyperscan\n", length);
 
-    if (hs_scan(database, inputData, length, 0, scratch, eventHandler,
-                pattern) != HS_SUCCESS) {
+    size_t matchCount = 0;
+    if (hs_scan(database, inputData.c_str(), length, 0, scratch, eventHandler,
+                &matchCount) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to scan input buffer. Exiting.\n");
         hs_free_scratch(scratch);
-        free(inputData);
         hs_free_database(database);
         return -1;
     }
-
+    std::cout<<"match count "<<matchCount<<std::endl;
     /* Scanning is complete, any matches have been handled, so now we just
      * clean up and exit.
      */
     hs_free_scratch(scratch);
-    free(inputData);
     hs_free_database(database);
     return 0;
 }
